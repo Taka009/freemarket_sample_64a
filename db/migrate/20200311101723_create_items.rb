0ebc0d9dd8ca8d0c8c36ea61838e4056084200e8
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-03-11 19:50:50 +0900

    変更修正

--- db/migrate/20200311101723_create_items.rb
+++ db/migrate/20200311101723_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-      t.string :name
-      t.string :description
-      t.prace :integer
+      t.name :string
+      t.description :string
+      t.integer :prace
       t.timestamps
     end
   end
