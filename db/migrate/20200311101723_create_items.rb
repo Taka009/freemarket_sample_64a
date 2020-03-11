6345652e6cff628d41472ba281d250eb23779a3e
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-03-11 19:54:18 +0900

    price

--- db/migrate/20200311101723_create_items.rb
+++ db/migrate/20200311101723_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.0]
     create_table :items do |t|
       t.string :name
       t.string :description
-      t.integer :prace
+      t.integer :price
       t.timestamps
     end
   end
