33f14e5b3cd57752ab4ecdb37bda4d48c136c23e
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-03-11 20:01:07 +0900

    chamges

--- db/migrate/20200311101723_create_items.rb
+++ db/migrate/20200311101723_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-      t.string :name
-      t.string :description
-      t.integer :price
+      t.string :name, null: false
+      t.string :description, null: false
+      t.integer :price, null:  false
       t.timestamps
     end
   end
