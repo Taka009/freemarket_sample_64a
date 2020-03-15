faaca20222f93fc2a475d76a0512b8f858915d49
kota930 <fuji-whereismygi210@softbank.ne.jp>
2020-03-15 18:26:30 +0900

    edited_button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sell/sell_form'
+  post 'items', to: 'items#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
