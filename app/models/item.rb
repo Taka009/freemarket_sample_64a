461a4f3044e86a16593b09b4d264aba0d670c768
kota930 <fuji-whereismygi210@softbank.ne.jp>
2020-03-21 15:24:16 +0900

    ediited_modelItem

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belongs_to :category
+  belongs_to :brand
+  belongs_to :postage
+  belongs_to :user
+  belongs_to :evaluation
+  has_many :images
+  has_many :goods
+  enum condition: [ :unused, :near_unused, :no_scratch, :slightly_scratched, :scratched, :shoddy]
+  enum shipping_day: [ :1_2, :2_3, :4_7]
+  enum dealing_stage: [ :exhibiting, :trading, :sold]
 end
