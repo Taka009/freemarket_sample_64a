c6564b5ec42aa767e99c8c324998aa2d31c9ef79
kota930 <fuji-whereismygi210@softbank.ne.jp>
2020-03-22 17:04:16 +0900

    change_name
    sell_form => sell

--- db/migrate/20200311101723_create_items.rb
+++ db/migrate/20200311101723_create_items.rb
@@ -3,7 +3,18 @@ class CreateItems < ActiveRecord::Migration[5.0]
     create_table :items do |t|
       t.string :name, null: false
       t.string :description, null: false
+      t.text :description, null: false
+      t.references :user, null: false foreign_key: true
+      t.references :seller, null: false, foreign_key: { to_table: :users }
+      t.references :buyer, foreign_key: { to_table: :users }
+      t.references :category, null: false foreign_key: true
+      t.references :brand, null: false foreign_key: true
+      t.references :postage, null: false foreign_key: true
+      t.references :evaluation, null: false foreign_key: true
       t.integer :price, null:  false
+      t.integer :condition, null:  false
+      t.integer :dealing_stage, null:  false
+      t.integer :shipping_day, null:  false
       t.timestamps
     end
   end
