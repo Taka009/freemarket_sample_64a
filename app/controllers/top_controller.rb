ea3ca25f46e28c36de6b26af975bfb9fb375a31a
takahumi0806 <yabuta0439@hotmail.com>
2020-03-25 02:28:59 +0900

    product_list

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
+    @categories = Category.eager_load(children: :children).where(parent_id: nil) 
     @items = Item.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'top/index'
   root "top#index"
   resources :items
+  resources :categories
 end
