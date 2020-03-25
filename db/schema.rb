b75ccb92d249b958ab87efe064c66d319f32653a
GHOST-FACE-TLI <phantom.clown.jester@gmail.com>
2020-03-25 19:03:27 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200324124904) do
+ActiveRecord::Schema.define(version: 20200325095846) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
-    t.string "ancestry"
+    t.string  "name"
+    t.string  "ancestry"
+    t.integer "parent_id"
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
