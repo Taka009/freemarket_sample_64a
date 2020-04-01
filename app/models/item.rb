bcc009882547b01924e9b1f4339dc3308fa1e298
kota930 <fuji-whereismygi210@softbank.ne.jp>
2020-04-02 05:05:37 +0900

    reset 02

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :condition_ls
   has_many :images, development :destroy
   acceots_nested_attributes_for :images, allow_destroy: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :condition_ls
 end
