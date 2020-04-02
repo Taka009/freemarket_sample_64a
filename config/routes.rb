e29f21d27adcf76cdaa6a52a7d0796cf50a6ab15
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-04-02 21:38:42 +0900

    s

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'sell/sell'
+  post 'items', to: 'items#create'
+  resources :sells
 
   post 'sell/sell'
   resources :sell, only: [:index, :new, :create]
 
   #post 'items', to: 'items#create'
-  #resources :sells
-  
+  #resources :sells  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
