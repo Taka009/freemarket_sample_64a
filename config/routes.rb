b8f382f2693ef0d4ce52c72c44ac65e9d1143aec
kota930 <fuji-whereismygi210@softbank.ne.jp>
2020-04-03 21:00:02 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: "dummy_items#index"
   devise_for :users
 
-  root 'sell#sell'
   get 'sell/sell'
   post 'sell/sell'
   resources :sell, only: [:index, :new, :create]
