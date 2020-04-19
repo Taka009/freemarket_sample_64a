6ed828910ccf45d9455a24977d0960de2ecca06a
takahumi0806 <yabuta0439@hotmail.com>
2020-04-19 22:40:13 +0900

    表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,9 +14,9 @@ class Item < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to_active_hash :conditionls
-    belongs_to_active_hash :shippingpayerls
+    belongs_to_active_hash :condition
+    belongs_to_active_hash :shippingpayer
     belongs_to_active_hash :prefecture
-    belongs_to_active_hash :shippingduration
+    belongs_to_active_hash :shipping_day
     belongs_to_active_hash :status
 end
