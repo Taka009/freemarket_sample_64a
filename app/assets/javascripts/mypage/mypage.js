3ac4a4fa434f9d7ab41dd3ca5e9219a54751c42a
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-04-26 19:08:07 +0900

    edited

---
+++ app/assets/javascripts/mypage/mypage.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function() {
+  /* タブの切替 （おしらせ、やることリスト）*/
+  $('.mypage-tab').on('click', function () {
+    $('.mypage-tab').removeClass('active');
+    $(this).addClass('active');
+  });
+  /* タブの切替 （取引中、過去の取引）*/
+  $('.btn').on('click', function () {
+    $('.btn').removeClass('action');
+    $(this).addClass('action');
+  });
+
+});
