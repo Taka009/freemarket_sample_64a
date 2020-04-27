4a050325b8a1a015ddea5fa6dd9747e9cfdcc2dc
kota930 <fuji-whereismygi210@softbank.ne.jp>
2020-04-28 01:56:00 +0900

    edit

--- app/helpers/custom_form_builder.rb
+++ app/helpers/custom_form_builder.rb
@@ -16,9 +16,11 @@ class CustomFormHelper < ActionView::Helpers::FormBuilder
     return super if options[:no_errors]
     super + pick_errors(attribute)
   end
+
   def text_area(attribute, options={})
     return super if options[:no_errors]
     super + pick_errors(attribute)
   end
+
 
 end
