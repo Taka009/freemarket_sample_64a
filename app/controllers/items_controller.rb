09edfeeedd460ff1f6baef8f83567b0b3cc968a7
kota930 <fuji-whereismygi210@softbank.ne.jp>
2020-05-05 13:46:20 +0900

    k

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,19 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
   def get_category_children
     respond_to do |format|
       format.html
