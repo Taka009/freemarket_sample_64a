4c814f22b47241d147f9e2e383bf30b00ce7966c
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-05-06 17:28:05 +0900

    変更修正

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,2 +1,11 @@
 class MypageController < ApplicationController
+
+  def index
+    @categories = Category.where(ancestry: nil)
+  end
+
+  def destroy
+    session[:keep_signed_out] = true
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,6 @@ Rails.application.routes.draw do
       post 'delete', to: 'credits#delete'
     end
   end
+  resources :mypage
 end
 
