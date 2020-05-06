89f65046344ee6391ab6b8d79d76de384d8c35bc
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-05-06 19:08:53 +0900

    change

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,11 +1,16 @@
 class MypageController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @categories = Category.where(ancestry: nil)
+    @users = current_user.id
+    @users = User.select("nickname")
+    @items = Item.all
   end
 
   def destroy
     session[:keep_signed_out] = true
   end
-  
+
 end
