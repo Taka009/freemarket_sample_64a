8b45db9ef2e89e1bf951a8f335a010d4eba57f23
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-05-07 14:00:01 +0900

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :kana_last_name, :kana_first_name, :birth_date])
   end
+  
 end
