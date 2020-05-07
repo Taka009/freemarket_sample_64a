d8c57fbaa7ecfaa7ddf1200a4023cb61985a56b2
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-05-07 11:38:43 +0900

    javascriptがリロード前でも発火するようにする

--- app/assets/javascripts/mypage/mypage.js
+++ app/assets/javascripts/mypage/mypage.js
@@ -1,14 +1,13 @@
-window.addEventListener('load', function() {
-  /* タブの切替 （おしらせ、やることリスト）*/
-  $('.mypage-tab').on('click', function () {
-    $('.mypage-tab').removeClass('active');
-    $(this).addClass('active');
-  });
+document.addEventListener("turbolinks:load", function() {
+    /* タブの切替 （おしらせ、やることリスト）*/
+    $('.mypage-tab').on('click', function () {
+      $('.mypage-tab').removeClass('active');
+      $(this).addClass('active');
+    });
 
-  /* タブの切替 （取引中、過去の取引）*/
-  $('.btm').on('click', function () {
-    $('.btm').removeClass('action');
-    $(this).addClass('action');
-  });
-  
+    /* タブの切替 （取引中、過去の取引）*/
+    $('.btm').on('click', function () {
+      $('.btm').removeClass('action');
+      $(this).addClass('action');
+    });
 });
